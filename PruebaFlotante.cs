296805d238afb819c03d9f838ab2d9d54a27a98a
Ailyn K. Zaragoza Hernandez <120075166+karyzh@users.noreply.github.com>
2023-07-07 13:32:06 -0600

    Add files via upload

---
+++ PruebaFlotante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaPuntoFlotante
+{
+    internal class PruebaFlotante
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine($"El salario de Pepe es de {salario} pesos");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ While.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_EstructuraRepeticionWhile
+{
+    internal class While
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejecutando el proyecto 12 - Estructura repetición While");
+
+            double saldo = 1000;
+            double interes = 0.75 / 100; //0.75% por mes.
+            int iNumMes = 1;
+
+            // MIENTRAS QUE ( CONDICIÓN )
+            while (iNumMes <= 12)
+            {
+                //saldo = saldo + saldo * interes; // 1000 + 1000 * 0.75 / 100
+                saldo += saldo * interes; //para que se vea mejor
+                Console.WriteLine($"El saldo luego del {iNumMes} mes {saldo}");
+                
+                // contadores
+                iNumMes++;
+                //iNumMes = iNumMes + 1;
+                //iNumMes = +1;
+            }
+            
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
