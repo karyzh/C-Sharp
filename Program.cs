eebc299a8eaadbf6ac8cab6075f66263320bc624
Ailyn K. Zaragoza Hernandez <120075166+karyzh@users.noreply.github.com>
2023-07-07 13:13:55 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            valor = 24 * 3 + 10;
+            Console.WriteLine(valor);
+            //1era forma de concatenar
+            Console.WriteLine("El resultado de la operación es " + valor);
+
+            //2da forma de concatenar
+            Console.WriteLine($"El resultado de la operación es {valor}");
+
+            //3era forma de concatenar
+            Console.Write("El resultado de la operación es ");
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+        }
+    }
+}
