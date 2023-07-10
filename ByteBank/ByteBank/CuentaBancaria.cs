8f32a6ec4a1a31fe2474d0db3a5136ba2b1f52e4
Ailyn K. Zaragoza Hernandez <lina26.kz4@gmail.com>
2023-07-09 20:08:53 -0600

    Commit

---
+++ ByteBank/ByteBank/CuentaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class CuentaBancaria
+    {
+        //Propiedades
+        public string nombreCliente;
+        public string numeroCuenta;
+        public string numeroAgencia;
+        public double saldo;
+        public double tasaInteres;
+        public double limiteSobregiro;
+
+        //Métodos
+        public bool RetirarDinero(double valorRetirar)
+        {
+            if (saldo < valorRetirar)
+            {
+                Console.WriteLine("No hay saldo suficiente");
+                return false;
+            } else if(valorRetirar <= 0)
+            {
+                Console.WriteLine("El valor a retirar debe ser mayor a 0");
+                return false;
+            } 
+
+            //saldo = saldo - valorRetirar;
+            saldo-= valorRetirar;
+            return true;
+
+        }
+
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,47 @@
+﻿using ByteBank;
+
+CuentaBancaria cuentaKaryme = new CuentaBancaria();
+
+cuentaKaryme.nombreCliente = "Karyme";
+cuentaKaryme.numeroCuenta = "0129 0133 3930 1382";
+cuentaKaryme.numeroAgencia = "0132";
+cuentaKaryme.saldo = 1254.65;
+cuentaKaryme.tasaInteres = 7.5;
+cuentaKaryme.limiteSobregiro = 500.00;
+Console.WriteLine("Aplicación de cuentas bancarias!");
+
+
+Console.WriteLine($"Saldo antes del retiro {cuentaKaryme.saldo}");
+
+bool retiroHecho = cuentaKaryme.RetirarDinero(120);
+if (retiroHecho)
+
+    Console.WriteLine($"Saldo luego del retiro {cuentaKaryme.saldo}");
+ else
+    Console.WriteLine("No fue posible hacer el retiro");
+
+
+if(cuentaKaryme.RetirarDinero(-40))//aquí no se podrá porque debe ser mayor a 0
+    Console.WriteLine($"Saldo luego del retiro {cuentaKaryme.saldo}");
+else
+    Console.WriteLine("No fue posible hacer el retiro de -40");
+
+if(cuentaKaryme.RetirarDinero(1999))
+    Console.WriteLine($"Saldo luego del retiro: {cuentaKaryme.saldo}");
+else
+    Console.WriteLine("No fue posible hacer el retiro de 2000");
+
+
+
+
+
+
+////variables por valor
+//string nombre1 = "Ailyn";
+//string nombre2 = "Ailyn";
+
+//Console.WriteLine("Variable: ");
+//Console.WriteLine(nombre1 == nombre2);
+
+
+Console.ReadLine();
